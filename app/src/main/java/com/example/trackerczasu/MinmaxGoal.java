62364a90135700b0bd06f25f90e04116930d9f47
unitax13 <theunitax@gmail.com>
2020-04-22 01:00:26 +0200

    klasa goal i minmaxgoal z konstruktorami

--- app/src/main/java/com/example/trackerczasu/MinmaxGoal.java
+++ app/src/main/java/com/example/trackerczasu/MinmaxGoal.java
@@ -1,4 +1,46 @@
 package com.example.trackerczasu;
 
 public class MinmaxGoal extends Goal {
+
+    public long min;
+    public long max;
+
+    public boolean isMin;
+    public boolean isMax;
+
+    MinmaxGoal(String minOrMaxIndicator, long minOrMax)
+    {
+        if (minOrMaxIndicator == "max")
+            {
+            this.max = minOrMax;
+            isMin = false;
+            isMax = true;
+            }
+        else
+            {
+            this.min = minOrMax;
+            isMin = true;
+            isMax = false;
+            }
+
+    }
+
+    public void changeMin()
+    {
+
+    }
+    public void changeMax()
+    {
+
+    }
+    public void deleteMin()
+    {
+
+    }
+    public void deleteMax()
+    {
+
+    }
 }
+
+
