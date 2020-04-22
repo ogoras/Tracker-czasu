7f405b8ad8bf87b3d50c200cdf2870b3cd63b97a
matlaczj <56789773+matlaczj@users.noreply.github.com>
2020-04-22 12:19:37 +0200

    Fixed app not launching

--- app/src/main/java/com/example/trackerczasu/MainActivity.java
+++ app/src/main/java/com/example/trackerczasu/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage(v);
+            }
+        });
     }
     public static TActivity testActivity;
 
