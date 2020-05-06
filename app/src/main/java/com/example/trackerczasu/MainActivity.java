53f035bc4d9b53bea18d78914df7bdf89ed6cb69
Skokus <skok2022@gmail.com>
2020-05-06 14:57:14 +0200

    Pobrana biblioteka recycleview

--- app/src/main/java/com/example/trackerczasu/MainActivity.java
+++ app/src/main/java/com/example/trackerczasu/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.trackerczasu;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 public class MainActivity extends AppCompatActivity {
