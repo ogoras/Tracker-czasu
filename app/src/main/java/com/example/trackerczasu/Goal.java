a417ac1bd9851c66d6d2273268cd81a53437bee5
matlaczj <56789773+matlaczj@users.noreply.github.com>
2020-05-06 17:39:04 +0200

    rozpisanie Goals, GoalList, nowe okno GoalsActivity.java

--- app/src/main/java/com/example/trackerczasu/Goal.java
+++ app/src/main/java/com/example/trackerczasu/Goal.java
@@ -3,27 +3,21 @@ package com.example.trackerczasu;
 public class Goal {
     public String name;
     public String comment;
-
-    public boolean isAchieved;
+    public boolean isAchieved = false;
     private boolean isPeriodic;
-
-
     private long creationTime;
     public long timeSpan;
-
-  //  ActivityType [] targetTypes;
+    public ActivityType targetType; //changed to single type instead of an array (no sense)
 
     Goal()
     {
         creationTime = System.currentTimeMillis()/1000;
     }
-
     Goal (String name)
     {
         this.name =  name;
         creationTime = System.currentTimeMillis()/1000;
     }
-
     Goal (String name, String comment)
     {
         this.name =  name;
@@ -31,34 +25,34 @@ public class Goal {
         creationTime = System.currentTimeMillis()/1000;
     }
 
-    public void addType()
+    public void addType(ActivityType activityType)
     {
-
+        this.targetType = activityType;
     }
-
     public void deleteType()
     {
-
+        this.targetType = null;
     }
-
-    public void editName()
+    public void editName(String name)
     {
-
+        this.name = name;
     }
-
-    public void editPeriodic()
+    public void editPeriodic(boolean isPeriodic)
     {
-
+        this.isPeriodic = isPeriodic;
     }
-
-    public void editDuration()
+    public void editDuration(long timeSpan)
     {
-
+        this.timeSpan = timeSpan;
     }
-
-    public void editComment()
+    public void editComment(String comment)
     {
-
+        this.comment = comment;
+    }
+    public boolean updateIsAchieved() // update variable to reflect the truth
+    {
+        ;
+        return true;
     }
 
 }
---
+++ app/src/main/java/com/example/trackerczasu/GoalsActivity.java
@@ -0,0 +1,16 @@
+package com.example.trackerczasu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoalsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goals);
+
+        // here will be screen with our goals and an option to create a new one
+    }
+}
