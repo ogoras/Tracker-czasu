40f932340a9fbd0b747d3724708ab7fc3eaf71ac
Szymon Kuś <skus30@wp.pl>
2020-05-17 21:42:06 +0200

    insertActivity method

--- app/src/main/java/com/example/trackerczasu/UserActivities.java
+++ app/src/main/java/com/example/trackerczasu/UserActivities.java
@@ -40,6 +40,34 @@ public class UserActivities implements Serializable {
             size--;
     }
 
+    public void insertActivity(String name, long startTime, long endTime){
+        if (endTime <= startTime) {
+            throw new IllegalArgumentException("end time must be bigger than start time");
+        }
+        if (size == 0){
+            if (endTime <= System.currentTimeMillis()/1000) {
+                addActivity(new TActivity(name, startTime, endTime));
+                return;
+            }
+            else throw new IllegalArgumentException("specified time span isn't inside a free space");
+        }
+        if (endTime <= list.get(0).startTime) {
+            list.add(0, new TActivity(name, startTime, endTime));
+            size++;
+            return;
+        }
+        for (int i = 0; i<size-1; i++){
+            if (startTime >= list.get(i).endTime && endTime <= list.get(i+1).startTime) {
+                list.add(i + 1, new TActivity(name, startTime, endTime));
+                size++;
+                return;
+            }
+        }
+        if (getCurrentActivity()==null && startTime >= list.get(size-1).endTime && endTime <= System.currentTimeMillis()/1000)
+            addActivity(new TActivity(name, startTime, endTime));
+        else throw new IllegalArgumentException("specified time span isn't inside a free space");
+    }
+
     public TActivity splitActivity(TActivity ActivityToSplit, long splitTime) // zwraca pierwszą połowę, a drugą połowę przypisuje do ActivityToSplit
     {
         if (splitTime <= ActivityToSplit.startTime || splitTime >= ( ActivityToSplit.isCurrent ? System.currentTimeMillis()/1000 : ActivityToSplit.endTime ) )
