6df0d406705605e62dfdee1f541b4d7ad2f05e13
unitax13 <61455951+unitax13@users.noreply.github.com>
2020-05-20 14:27:36 +0200

    adding stuff

--- app/src/main/java/com/example/trackerczasu/ui/main/GoalEdit.java
+++ app/src/main/java/com/example/trackerczasu/ui/main/GoalEdit.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.trackerczasu.R;
 
@@ -15,11 +16,14 @@ import com.example.trackerczasu.R;
 
 public class GoalEdit extends AppCompatActivity {
 
+    EditText exactNameOfActivityType;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_goal_edit);
+        exactNameOfActivityType = (EditText) findViewById(R.id.exactNameEditText);
 
 
     }
