678c893e91d15efb78f283da9b8fc01ca5ff9610
Szymon Kuś <skus30@wp.pl>
2020-05-20 14:33:31 +0200

    generate tag colors from hash code

--- app/src/main/java/com/example/trackerczasu/ui/main/ActivitiesAdapter.java
+++ app/src/main/java/com/example/trackerczasu/ui/main/ActivitiesAdapter.java
@@ -228,7 +228,10 @@ public class ActivitiesAdapter extends RecyclerView.Adapter {
             holder1.name.setText(tActivity.type);
             Paint paint = holder1.name.getPaint();
             holder1.name.setWidth((int) paint.measureText(tActivity.type));
-            holder1.tag.setText(tActivity.tag);
+            if (tActivity.tag != null) {
+                holder1.tag.setText(tActivity.tag);
+                holder1.tag.setBackgroundColor(tActivity.tag.hashCode() & 0xFFFFFF);
+            }
             holder1.name.setTextColor(typeList.findType(tActivity.type).getColor());
 
             switch (holder.getItemViewType()){
