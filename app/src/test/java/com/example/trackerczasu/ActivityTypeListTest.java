2930fdf2baf284bafae1c960127b79f90cb02915
Skokus <skok2022@gmail.com>
2020-06-03 20:16:47 +0200

    Test DeleteType

--- app/src/test/java/com/example/trackerczasu/ActivityTypeListTest.java
+++ app/src/test/java/com/example/trackerczasu/ActivityTypeListTest.java
@@ -15,9 +15,20 @@ public class ActivityTypeListTest {
             assertEquals(t.name, list.get(0).name);
             assertEquals(t.icon, list.get(0).icon);
     }
+
+    @Test
+    public void deleteType() {
+        ActivityTypeList list = new ActivityTypeList();
+        list.ActivityTypes.add(new ActivityType("test", R.drawable.music));
+        list.deleteType(list.get(1));
+        assertNotNull(list);
+        assertEquals(0,list.size);
+    }
     /*If you run a test that calls an API from the Android SDK that you do not mock,
     you'll receive an error that says this method is not mocked.
     That's because the android.jar file used to run unit tests does not contain any actual code
     (those APIs are provided only by the Android system image on a device).*/
+
+
 }
 
