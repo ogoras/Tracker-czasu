72456b7dfd9f1f611a2838e5e7fb0bfbc7d26968
unitax13 <61455951+unitax13@users.noreply.github.com>
2020-06-04 11:41:06 +0200

    added 2 methods of InsertActivityTest

--- app/src/test/java/com/example/trackerczasu/InsertActivityTest.java
+++ app/src/test/java/com/example/trackerczasu/InsertActivityTest.java
@@ -16,4 +16,26 @@ public class InsertActivityTest {
 
         userActivities.insertActivity("cde",37199,37200);
     }
+
+    @Test
+    public void insertActivity2() {
+        UserActivities userActivities = new UserActivities();
+        TActivity tActivity = new TActivity("bcd", 37100, 37200);
+        userActivities.addActivity(tActivity);
+        tActivity = new TActivity("bcd", 37201, 37220);
+        userActivities.addActivity(tActivity);
+
+        userActivities.insertActivity("cde",37200,37201);
+
+    }
+    @Test (expected = IllegalArgumentException.class)
+    public void insertActivity3() {
+        UserActivities userActivities = new UserActivities();
+        TActivity tActivity = new TActivity("bcd", 37100, 37200);
+        userActivities.addActivity(tActivity);
+        tActivity = new TActivity("bcd", 37201, 37220);
+        userActivities.addActivity(tActivity);
+
+        userActivities.insertActivity("cde",37201,372202);
+    }
 }
